0dc21bd5c699cd2ed28d89c5655a66345a9cc2c7
Michael Cohen <michaeldalecohen@gmail.com>
2016-01-13 18:12:28 +1100

    First Commit
    Final Project for FEWD21 class

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function() {
+	$("#owl-slider").owlCarousel({
+	 
+		navigation : true, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem : true,
+		autoHeight : true,
+		// autoPlay : 5000
+	 
+  	});
+	
+});
