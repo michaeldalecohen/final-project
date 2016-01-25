dc1375cd71b065b752c979445205c51aadbcd89c
Michael Cohen <michaeldalecohen@gmail.com>
2016-01-26 00:59:40 +1100

    Final Submission
    FEWD21 Final Project.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
 $(function() {
+
+	$(document).on("scroll",function(){
+	    if($(document).scrollTop()>450){
+	        $("header").removeClass("masthead").addClass("masthead-small");
+	    } else{
+	        $("header").removeClass("masthead-small").addClass("masthead");
+	    }
+	});
+
+	$('a').click(function(){
+	    $('html, body').animate({
+	        scrollTop: $( $.attr(this, 'href') ).offset().top
+	    }, 500);
+	    return false;
+	});
+
 	$("#owl-slider").owlCarousel({
 	 
 		navigation : true, // Show next and prev buttons
@@ -7,74 +23,93 @@ $(function() {
 		singleItem : true,
 		autoHeight : true,
 		navigationText: ['<span class="fa fa-chevron-left"></span>', '<span class="fa fa-chevron-right"></span>']
-	 
+		
   	});
 	
-		$(".content-carousel").owlCarousel({
-		 
-			navigation : false, // Show next and prev buttons
-			slideSpeed : 300,
-			paginationSpeed : 400,
-			singleItem : true,
-			autoHeight : true,
-			pagination: false
-			
-		 
-	  	});
-
-		var r31 = $(".content-carousel").data('owlCarousel');
-
-		$('#overview').on('click', function() {
-			r31.goTo(0);
-		});
-
-	  	$('#r31-specs').on('click', function() {
-	  		r31.goTo(2);
-	  	});
-
-	function filterPath(string) {
-	  return string
-	    .replace(/^\//,'')
-	    .replace(/(index|default).[a-zA-Z]{3,4}$/,'')
-	    .replace(/\/$/,'');
-	  }
-	  var locationPath = filterPath(location.pathname);
-	  var scrollElem = scrollableElement('html', 'body');
+	$(".content-carousel").owlCarousel({
 	 
-	  $('a[href*=#]').each(function() {
-	    var thisPath = filterPath(this.pathname) || locationPath;
-	    if (  locationPath == thisPath
-	    && (location.hostname == this.hostname || !this.hostname)
-	    && this.hash.replace(/#/,'') ) {
-	      var $target = $(this.hash), target = this.hash;
-	      if (target) {
-	        var targetOffset = $target.offset().top;
-	        $(this).click(function(event) {
-	          event.preventDefault();
-	          $(scrollElem).animate({scrollTop: targetOffset}, 400, function() {
-	            location.hash = target;
-	          });
-	        });
-	      }
-	    }
-	  });
-	 
-	  // use the first element that is "scrollable"
-	  function scrollableElement(els) {
-	    for (var i = 0, argLength = arguments.length; i <argLength; i++) {
-	      var el = arguments[i],
-	          $scrollElement = $(el);
-	      if ($scrollElement.scrollTop()> 0) {
-	        return el;
-	      } else {
-	        $scrollElement.scrollTop(1);
-	        var isScrollable = $scrollElement.scrollTop()> 0;
-	        $scrollElement.scrollTop(0);
-	        if (isScrollable) {
-	          return el;
-	        }
-	      }
-	    }
-	    return [];
-	  }
+		navigation : false, // Show next and prev buttons
+		slideSpeed : 300,
+		paginationSpeed : 400,
+		singleItem : true,
+		autoHeight : true,
+		pagination: false
+		
+  	});
+
+	var r31 = $("#r31-carousel").data('owlCarousel');
+
+	$('#r31-overview').on('click', function() {
+		r31.goTo(0);
+	});
+
+  	$('#r31-specs').on('click', function() {
+  		r31.goTo(1);
+  	});
+
+  	$('#r31-video').on('click', function() {
+  		r31.goTo(2);
+  	});
+
+  	var r32 = $("#r32-carousel").data('owlCarousel');
+
+	$('#r32-overview').on('click', function() {
+		r32.goTo(0);
+	});
+
+  	$('#r32-specs').on('click', function() {
+  		r32.goTo(1);
+  	});
+
+  	$('#r32-video').on('click', function() {
+  		r32.goTo(2);
+  	});
+
+  	var r33 = $("#r33-carousel").data('owlCarousel');
+
+	$('#r33-overview').on('click', function() {
+		r33.goTo(0);
+	});
+
+  	$('#r33-specs').on('click', function() {
+  		r33.goTo(1);
+  	});
+
+  	$('#r33-video').on('click', function() {
+  		r33.goTo(2);
+  	});
+
+  	var r34 = $("#r34-carousel").data('owlCarousel');
+
+	$('#r34-overview').on('click', function() {
+		r34.goTo(0);
+	});
+
+  	$('#r34-specs').on('click', function() {
+  		r34.goTo(1);
+  	});
+
+  	$('#r34-video').on('click', function() {
+  		r34.goTo(2);
+  	});
+
+  	var r35 = $("#r35-carousel").data('owlCarousel');
+
+	$('#r35-overview').on('click', function() {
+		r35.goTo(0);
+	});
+
+  	$('#r35-specs').on('click', function() {
+  		r35.goTo(1);
+  	});
+
+  	$('#r35-video').on('click', function() {
+  		r35.goTo(2);
+  	});
+
+  	$('.btn').on('click', function() {
+  		$('.btn').removeClass('btn-active');
+  		$(this).toggleClass('btn-active');
+  	});
+
 });
